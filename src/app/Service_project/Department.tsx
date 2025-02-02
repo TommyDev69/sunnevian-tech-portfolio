8d3af798d6f3b081f5cb151186e78100c166abdf
TommyDev69 <sunnevian4life@gmail.com>
2025-02-02 23:41:39 +0100

    port

--- src/app/Service_project/Department.tsx
+++ src/app/Service_project/Department.tsx
@@ -46,9 +46,9 @@ const Department = () => {
     },
   ])
     return (
-        <div className="md:pl-0 pl-10 mt-32 ">
+        <div className="md:pl-0 pdl-10 mt-32 ">
            
-                <h2 className="text-white text-center text-3xl md:text-[45px] pb-14 capitalize">
+                <h2 className="text-white text-center text-2xl md:text-[40px] pb-14 capitalize">
                    The Service We Provide For You
                 </h2>
             
