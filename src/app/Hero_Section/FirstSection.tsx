84b39ee49fe34f431a757128df7d7b580dd09cbc
TommyDev69 <sunnevian4life@gmail.com>
2025-02-03 01:19:23 +0100

    port

--- src/app/Hero_Section/FirstSection.tsx
+++ src/app/Hero_Section/FirstSection.tsx
@@ -1,10 +1,10 @@
 const FirstSection = () => {
     return ( 
         <div className=" md:w-1/2 flex flex-col py-10 md:px-10 px-0  ">
-            <h2 className="w-[40%w] text-white text-6xl text-center capitalize md:text-left leading-[50px] md:leading-[69px] md:text-[50px]  font-bold">
+            <h2 className="w-[40%w] text-white text-4xl text-center capitalize md:text-left leading-[50px] md:leading-[69px] md:text-[40px]  font-bold">
                 build your awesome platform.
             </h2>
-            <p className="text-white mt-10 text-3xl md:text-[22px] leading-[24px] md:leading-[30px]">
+            <p className="text-white mt-10 text-2xl md:text-[16px] leading-[36px] md:leading-[36px]">
                 Sunnevian Tech Studio is a digital studio that offers a variety of services, including UI/UX design for developers, web design and development for clients, and collaboration with developers. We are committed to providing the best service to all those who use our offerings.
             </p>
             <div className="mt-32">
--- src/app/Hero_Section/MainHero.tsx
+++ src/app/Hero_Section/MainHero.tsx
@@ -4,7 +4,7 @@ import SecondSection from './SecondSection';
 
 function MainHero() {
   return (
-    <div className='contai3ner mt-10 flex space-y-6 flex-col-reverse mx-auto  md:flex-row px-14'>
+    <div className='contai3ner mt-10 flex space-y-6 flex-col-reverse mx-auto  md:flex-row px-6 md:px-14'>
       <FirstSection />
       <SecondSection />
     </div>
