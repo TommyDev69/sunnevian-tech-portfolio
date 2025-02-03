ba71d3a9232cf812b55880abcf8c7bd4e24413b4
TommyDev69 <sunnevian4life@gmail.com>
2025-02-03 01:29:48 +0100

    wifi

--- src/app/Hero_Section/FirstSection.tsx
+++ src/app/Hero_Section/FirstSection.tsx
@@ -1,7 +1,7 @@
 const FirstSection = () => {
     return ( 
         <div className=" md:w-1/2 flex flex-col py-10 md:px-10 px-0  ">
-            <h2 className="w-[40%w] text-white text-4xl text-center capitalize md:text-left leading-[50px] md:leading-[69px] md:text-[40px]  font-bold">
+            <h2 className="text-white text-4xl text-center capitalize md:text-left leading-[50px] md:leading-[69px] md:text-[40px]  font-bold">
                 build your awesome platform.
             </h2>
             <p className="text-white mt-10 text-2xl md:text-[16px] leading-[36px] md:leading-[36px]">
@@ -9,7 +9,7 @@ const FirstSection = () => {
             </p>
             <div className="mt-32">
 
-            <a href="" className="py-6 px-10 rounded-2xl  font-bold text-3xl text-white border-2 capitalize">our service</a>
+            <a href="" className="py-6 px-8 rounded-2xl  font-bold text-2xl md:text-[16px] text-white border-2 capitalize">our service</a>
             </div>
         </div>
      );
--- src/app/feature_section/MainFeatures.tsx
+++ src/app/feature_section/MainFeatures.tsx
@@ -7,10 +7,10 @@ import Scanner from "./Scanner";
 
 export default function MainFeatures() {
   return (
-    <div className=" md:px-0 px-6 flex flex-col mt-10 mx-auto space-y-6 md:space-y-0">
+    <div className=" md:px-0 px-4 flex flex-col mt-10 mx-auto space-y-6 md:space-y-0">
       <div className="py-10">
-        <h1 className="text-white w-ful md: text-center  font-bold text-2xl md:text-[35px] capitalize py-10">
-          motivation and evolution of a commputer age{" "}
+        <h1 className="text-white text-center leading-[50px] md:leading-[69px]  font-bold text-4xl md:text-[35px] capitalize py-10">
+          motivation and evolution of a commputer age
         </h1>
       </div>
       <div className=" md:px-14 px-0 flex flex-col mt-4 mx-auto space-y-10  md:space-y-0 md:flex-row">
