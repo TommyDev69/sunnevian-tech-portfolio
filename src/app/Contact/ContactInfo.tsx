1a6d4c0387aa47c2f43ece3f946ffbabd460f2d4
TommyDev69 <sunnevian4life@gmail.com>
2025-02-03 02:54:00 +0100

    plot

--- src/app/Contact/ContactInfo.tsx
+++ src/app/Contact/ContactInfo.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 export default function ContactInfo() {
   return (
-    <div className='md:w-1/2  flex justify-end '>
+    <div className='w-[36%]  flex justify-end '>
       <a href="" className="py-6 px-8 rounded-2xl  font-bold text-2xl md:text-[16px] text-white border-2 capitalize">our service</a>
     </div>
   )
--- src/app/Contact/MainContact.tsx
+++ src/app/Contact/MainContact.tsx
@@ -3,8 +3,8 @@ import ContactInfo from './ContactInfo'
 
 export default function MainContact() {
   return (
-    <div className='flex  space-x-12'>
-       <div className="px-6 w-1/2  bg-black ">
+    <div className='flex  space-x-12 px-6 border-dashed border-2'>
+       <div className="px-6 w-[75%]  bg-black ">
        <h1 className="text-white text-left md:text-[30px] text-3xl font-bold capitalize">
            Contact us for the service you want to use
        </h1>
