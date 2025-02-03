f74d8562301474b990cfc6803cb86065baf94c1c
TommyDev69 <sunnevian4life@gmail.com>
2025-02-03 01:05:13 +0100

    port

--- src/app/Service_project/Strength.tsx
+++ src/app/Service_project/Strength.tsx
@@ -20,15 +20,15 @@ const Strenght:React.FC<Material> = ({Tool}) => {
                 {Tool.map((subj) => (
                 <div className=" md:px-8 px-0  py-10" key={subj.id}>
                     
-                    <div className="md:w-[50%] mx-8 md:mx-auto">
+                  <div className="md:w-[50%] mx-8 md:mx-auto">
 
-                       <Image src={subj.Image} alt={`Project ${subj.id}`}  />
-                    </div>
-                    <div className=" md:w-full bg-blac">
+                      <Image src={subj.Image} alt={`Project ${subj.id}`}  />
+                  </div>
+                  <div className=" md:w-full ">
                     <h3 className="mt-4 text-[8px] md:text-3xl  font-bold text-center  text-white">{subj.heading}</h3>
                     
                     <p className="mt-2 text-center text-[10px] md:text-left md:text-[14px] text-white font-thin">{subj.content}</p>
-                    </div>
+                  </div>
                 </div>
             ))}
         </div>
