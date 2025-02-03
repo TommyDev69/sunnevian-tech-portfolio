79f7d5bd860cdf17531c0ba38269c58d35b77ce3
TommyDev69 <sunnevian4life@gmail.com>
2025-02-03 02:54:54 +0100

    port

--- src/app/Contact/MainContact.tsx
+++ src/app/Contact/MainContact.tsx
@@ -5,7 +5,7 @@ export default function MainContact() {
   return (
     <div className='flex  space-x-12 px-6 border-dashed border-2'>
        <div className="px-6 w-[75%]  bg-black ">
-       <h1 className="text-white text-left md:text-[30px] text-3xl font-bold capitalize">
+       <h1 className="text-white text-left md:text-[30px] text-2xl font-bold capitalize">
            Contact us for the service you want to use
        </h1>
        </div>
