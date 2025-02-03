b58b0247d3d95a657b022fdd22bda641aa94efe3
TommyDev69 <sunnevian4life@gmail.com>
2025-02-03 13:32:18 +0100

    port

--- src/app/Contact/ContactInfo.tsx
+++ src/app/Contact/ContactInfo.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 
 export default function ContactInfo() {
   return (
-    <div className='w-[36%]  flex justify-end '>
-      <a href="" className="py-6 px-8 rounded-2xl  font-bold text-2xl md:text-[16px] text-white border-2 capitalize">our service</a>
+    <div className='w-[36%]  flex justify-center items-center'>
+      <a href="" className="py-6 px-8 rounded-2xl  font-bold text-lg md:text-[16px] text-white border-2 capitalize">our service</a>
     </div>
   )
 }
--- src/app/Contact/MainContact.tsx
+++ src/app/Contact/MainContact.tsx
@@ -3,11 +3,11 @@ import ContactInfo from './ContactInfo'
 
 export default function MainContact() {
   return (
-    <div className='flex  space-x-12 px-6 border-dashed border-2'>
-       <div className="px-6 w-[75%]  bg-black ">
-       <h1 className="text-white text-left md:text-[30px] text-2xl font-bold capitalize">
-           Contact us for the service you want to use
-       </h1>
+    <div className='flex py-20  space-x-12 px-6 border-dashed border-2'>
+       <div className="p-6 w-[75%]  bg-black ">
+        <h1 className="text-white text-left md:text-[30px] text-2xl font-bold capitalize">
+            Contact us for the service you want to use
+        </h1>
        </div>
        <ContactInfo />
     </div>
